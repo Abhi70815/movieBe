da19d6a14d49ccf139ecc989f04bdf9f7d1b57f1
Abhi70815 <abhishekchurasia45621@gmail.com>
2025-01-24 12:11:55 +0530

    save

--- pages/auth.js
+++ pages/auth.js
@@ -1,100 +1,44 @@
-// import Head from "next/head";
 import Image from "next/image";
-// import { useSession, signIn, signOut } from "next-auth/react";
-// import { useRouter } from "next/router";
-// import Loading from "@/components/Loading";
-let signIn;
-let signOut;
+import Head from "next/head";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Loading from "@/components/Loading";
 
 export default function Auth() {
-  // const { data: session, status } = useSession();
-  // const router = useRouter();
+  const { data: session, status } = useSession();
 
-  // if (status === "loading") {
-  //   return <Loading />;
-  // }
-
-  // return (import Head from "next/head";
-  import Image from "next/image";
-  // import { useSession, signIn, signOut } from "next-auth/react";
-  // import { useRouter } from "next/router";
-  // import Loading from "@/components/Loading";
-  let signIn;
-  let signOut;
-
-  export default function Auth() {
-    // const { data: session, status } = useSession();
-    // const router = useRouter();
-
-    // if (status === "loading") {
-    //   return <Loading />;
-    // }
-
-    return (
-      <>
-        {/* <Head> */}
-          <title>Movie App | Backend</title>
-        {/* </Head> */}
-
-        <div className="container">
-          <div className="loginfront flex flex-center">
-            <div className="loginbox flex flex-col">
-              <Image src="/img/abhi.jpg" width={250} height={250} />
-              <h1>Welcome Admin of the Movie</h1>
-              <p>
-                Visit our main Website{" "}
-                <a href="https://www.linkedin.com/in/abhishek_chaurasia_1">
-                  abhishek Chaurasia
-                </a>
-              </p>
-              {
-                // session 
-                "hiii" ? (
-                  <button className="mt-2" onClick={signOut}>
-                    Log Out Here
-                  </button>
-                ) : (
-                  <button className="mt-2" onClick={signIn}>
-                    Log in with Google
-                  </button>
-                )}
-            </div>
-          </div>
-        </div>
-      </>
-    );
+  if (status === "loading") {
+    return <Loading />;
   }
 
-  <>
-    <Head>
-      <title>Movie App | Backend</title>
-    </Head>
+  return (
+    <>
+      <Head>
+        <title>Movie App | Backend</title>
+      </Head>
 
-    <div className="container">
-      <div className="loginfront flex flex-center">
-        <div className="loginbox flex flex-col">
-          <Image src="/img/abhi.jpg" width={250} height={250} />
-          <h1>Welcome Admin of the movie</h1>
-          <p>
-            Visit our main Website{" "}
-            <a href="https://www.linkedin.com/in/Abhshek_chaurasia_1">
-              Abhishek chaurasia
-            </a>
-          </p>
-          {
-            // session 
-            "hiii" ? (
-              <button className="mt-2" onClick={signOut}>
+      <div className="container">
+        <div className="loginfront flex flex-center">
+          <div className="loginbox flex flex-col">
+            <Image src="/img/abhi.jpg" width={250} height={250} />
+            <h1>Welcome Admin of the Movie</h1>
+            <p>
+              Visit our main Website{" "}
+              <a href="https://www.linkedin.com/in/abhishek_chaurasia_1">
+                Abhishek Chaurasia
+              </a>
+            </p>
+            {session ? (
+              <button className="mt-2" onClick={() => signOut()}>
                 Log Out Here
               </button>
             ) : (
-              <button className="mt-2" onClick={signIn}>
+              <button className="mt-2" onClick={() => signIn()}>
                 Log in with Google
               </button>
             )}
+          </div>
         </div>
       </div>
-    </div>
-  </>
-
+    </>
+  );
 }
